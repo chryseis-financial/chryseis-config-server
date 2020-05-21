71ad191a09f33d88c85a10e1b9d5e7d390112699
Balaji Varadharajan <vbalaji215@gmail.com>
2020-05-20 22:19:27 -0400

    Initial Commit

---
+++ src/test/java/com/chryseis/config/server/ChryseisConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chryseis.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChryseisConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
